9a08c98e181d57d7fc6d5ad8367f7806f0368a51
Tinusw <tinus.wagnersa@gmail.com>
2018-04-14 19:33:59 +0200

    adding react-router-dom, wrapping app in root route and starting on a header component.

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter, Route, browserHistory } from "react-router-dom";
 
-import App from './components/app';
-import reducers from './reducers';
+import App from "./components/app";
+import reducers from "./reducers";
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
 ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-, document.querySelector('.container'));
+    <BrowserRouter history={browserHistory}>
+      <Route path="/" component={App}></Route>
+    </BrowserRouter>
+  </Provider>,
+  document.querySelector(".container")
+);
