7edded8f94fe88b8b2a5d051ae4a1faa29053b3d
Tinusw <tinus.wagnersa@gmail.com>
2018-04-14 19:12:06 +0200

    initial setup and config

---
+++ README.md
@@ -0,0 +1,3 @@
+# React/Redux Client App
+
+Coming soon
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: ["es2015"]
+        },
+      },
+    ]
+  },
+  devServer: {
+    publicPath: '/',
+    port: 3000,
+    historyApiFallback: true
+  }
+};
