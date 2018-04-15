871fc33782c4569e89d9f4f8484df5f19c5a9e7e
Tinusw <tinus.wagnersa@gmail.com>
2018-04-15 20:40:37 +0200

    wiring up basics of our reducer.Creating a shared types file & cleaing up actions.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,24 +1,22 @@
-import axios from 'axios'
-import { browserHistory } from 'react-router'
-import history from '../modules/history'
+import axios from "axios";
+import history from "../modules/history";
 
-import { SUBMIT_LOGIN } from "./types";
+import { AUTH_USER, UNAUTH_USER } from "./types";
 
 const ROOT_URL = "http://localhost:";
 const PORT = "3030";
 
 export function signinUser({ email, password }) {
   return function(dispatch) {
-    axios.post(`${ROOT_URL}${PORT}/signin`, { email, password })
+    axios
+      .post(`${ROOT_URL}${PORT}/signin`, { email, password })
       .then(response => {
-        // return {
-        //   type: SUBMIT_LOGIN,
-        //   payload: response
-        // }
-        history.push('/feature')
+        console.log(response);
+        dispatch({ type: AUTH_USER });
+        history.push("/feature");
       })
-      .catch((error) => {
-        console.log(error)
-      })
+      .catch(error => {
+        console.log("NOOOOOOO", error);
+      });
   };
 }
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,3 @@
 export const SUBMIT_LOGIN = 'submit_login'
+export const AUTH_USER = 'auth_user'
+export const UNAUTH_USER = 'unauth_user'
