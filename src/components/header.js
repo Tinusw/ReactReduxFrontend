6e106b9247a7893e410ef4a311bb2f7884b46544
Tinusw <tinus.wagnersa@gmail.com>
2018-04-16 19:32:15 +0200

    upgraded navbar to container so authenticated state can help us conditionally render links.

--- src/components/header.js
+++ src/components/header.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
+import * as actions from "../actions";
 
-export default class header extends Component {
+class Header extends Component {
+  renderLinks() {
+    if (this.props.authenticated){
+      return (
+        <li className="nav-item">
+          <Link classname="nav-link" to="/signout">
+            Sign Out
+          </Link>
+        </li>
+      )
+    } else {
+      return [
+        <li className="nav-item" key={1}><Link className="nav-link" to="/signin">Sign in</Link></li>,
+        <li className="nav-item" key={2}><Link className="nav-link" to="/signup">Sign up</Link></li>
+      ]
+    }
+  }
+
   render() {
     return (
       <nav className="navbar navbar-light">
+        <Link to="/" className="navbar-brand">Home</Link>
         <ul className="nav navbar-nav">
-          <li className="nav-item">Sign in</li>
+          {this.renderLinks()}
         </ul>
       </nav>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return { authenticated: state.auth.authenticated };
+}
+
+export default connect(mapStateToProps)(Header);
